6b51fbaa604e3f84d4821e4f6d6649e126dcfa1a
KG <khatunaggg12345@gmail.com>
2023-12-30 01:03:22 +0400

    correction of title

--- src/components/title/Title.jsx
+++ src/components/title/Title.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import style from "./title.module.css";
 
 const Title = () => {
-  return <h1 className={style.title}>fiq</h1>;
+  return <h1 className={style.title}>faq</h1>;
 };
 
 export default Title;
